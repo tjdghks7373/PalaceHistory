500f610d30e0311906ab930cc84cd216439b387e
tjdghks7373 <tjdghks7373@naver.com>
2026-03-21 11:29:05 +0900

    fix: GitHub 아바타 이미지 도메인 허용

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -9,6 +9,10 @@ const nextConfig = {
         protocol: "https",
         hostname: "cdn.shopify.com",
       },
+      {
+        protocol: "https",
+        hostname: "avatars.githubusercontent.com",
+      },
     ],
   },
 };
